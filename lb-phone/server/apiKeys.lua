71bc01cc0ff4858aed487bea25c14d63b38ac8a8
p1once <pionce@pm.me>
2025-10-24 01:39:56 +0200

    update

---
+++ lb-phone/server/apiKeys.lua
@@ -0,0 +1,33 @@
+-- Webhook for instapic posts, recommended to be a public channel
+INSTAPIC_WEBHOOK = "https://discord.com/api/webhooks/1229821074041081876/xukSRQdEd4pVEU4vkWVCPE4I-UvRlIPWLfI9Sdtfqk7Aa5vGoNeCjIv12LNIWUh_bS8f"
+-- Webhook for birdy posts, recommended to be a public channel
+BIRDY_WEBHOOK = "https://discord.com/api/webhooks/1229821125345808464/u4YcODFUm0_r-POqcBLj3ez6yALGEVLztavtddULy79YxOY1z4MgNgoY1-0hXblGpEIm"
+
+-- Discord webhook for server logs
+LOGS = {
+    Calls = "https://discord.com/api/webhooks/1229821130827628606/O0D1BU-5iEAfzZVZ1kC2p6XXS9NVkWEeizBFIW9l54sNM4Gx67Yn2V3Uuh0WMzemIPvQ", -- set to false to disable
+    Messages = "https://discord.com/api/webhooks/1229821913032036444/dTCFb11UfstMQ3YDEjCC5jidJgDrEYl18sWtknjyfkBkiHKsGg4CTweuGBe_tbnd6beQ",
+    InstaPic = "https://discord.com/api/webhooks/1229821074041081876/xukSRQdEd4pVEU4vkWVCPE4I-UvRlIPWLfI9Sdtfqk7Aa5vGoNeCjIv12LNIWUh_bS8f",
+    Birdy = "https://discord.com/api/webhooks/1229821125345808464/u4YcODFUm0_r-POqcBLj3ez6yALGEVLztavtddULy79YxOY1z4MgNgoY1-0hXblGpEIm",
+    YellowPages = "https://discord.com/api/webhooks/1229821915166674965/jUiGU75F0BL7MQ1qwS--GBUI7N0SR-WkWLGQEgE7VA9i_dacYK8LC3a4TYc1S_XIMHcy",
+    Marketplace = "https://discord.com/api/webhooks/1229821917473669251/Vu1et5zND-D17ieyupuHnsBC703wq878bPMVSbpddgzCC9MkO_LRZmuS4pEOTno9rLAl",
+    Mail = "https://discord.com/api/webhooks/1229821919608438838/UH0joUDO8hGDqwKHT_RHPmO1MsacwTh7V5HB0W9K0g0rpZoawR9qDXv2KYEQA3V1xqTu",
+    Wallet = "https://discord.com/api/webhooks/1229821921756057611/Zgvem6LvfXx0Sdzba9HrjPMbG_-tEkVSHZG_hYwJXttMn0bIvNyT_WuWU6ceK5nUOMZZ",
+    DarkChat = "https://discord.com/api/webhooks/1229821923723055155/0Tj531F_UrPlFgkdlsjt_YMlVk6E7GpJ9tWw4f2_6Iy9542at6r0BF1TpLRkA6LMdeF1",
+    Services = "https://discord.com/api/webhooks/1229821926248153088/2DCitgpiKM2MJXuhKBfGM5vXv-kg0ls3V2_yXCQWODcWVMwe0ileMGIuTcpe2Qzx4Xnv",
+    Crypto = "https://discord.com/api/webhooks/1229821928282525778/y99ITJcgX49vgDMCfMavVR7CYXNdwrueqqrFPF1EiZR8fR-8WT4eg607CoLRL4HeQnT4",
+    Trendy = "https://discord.com/api/webhooks/1229821930920611922/Xs5kzdw0MPXKCyM7S2GluwHZB2piIZRq6luXb_Gzatt86q7KX8Uc4ppY47lZuvw6QiaR",
+    Uploads = "https://discord.com/api/webhooks/1229821932728488126/4vx0XO8dEcKepCYFFJBXvICvRgLWSA11cGv-UnelHFCfySMNNn-nr_a9kyalQ4F6GXJE" -- all camera uploads will go here
+}
+
+-- Set your API keys for uploading media here.
+-- Please note that the API key needs to match the correct upload method defined in Config.UploadMethod.
+-- The default upload method is Fivemanage
+-- We STRONGLY discourage using Discord as an upload method, as uploaded files may become inaccessible after a while.
+-- You can get your API keys from https://fivemanage.com/
+-- A video tutorial for how to set up Fivemanage can be found here: https://www.youtube.com/watch?v=y3bCaHS6Moc
+API_KEYS = {
+    Video = "gtSv9E7UsvWuREhPasx4cXajlK5yY3cX",
+    Image = "gtSv9E7UsvWuREhPasx4cXajlK5yY3cX",
+    Audio = "gtSv9E7UsvWuREhPasx4cXajlK5yY3cX",
+}
